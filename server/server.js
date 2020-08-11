1c46d7e1e6eceb118a5bd04f3bb2cbe721061ba1
Matt Meigs <mattmeigs@gmail.com>
2020-08-11 11:25:10 -0400

    dev dependencies

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './index.html'));
+})
+
+
+
+app.listen(3000);
