ff99485873de5a940fff73be4c88857effaee7e8
Matt Meigs <mattmeigs@gmail.com>
2020-10-24 16:03:11 -0400

    cleaned server file

--- server/server.js
+++ server/server.js
@@ -2,18 +2,17 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-
+// Require in routers //
 const eventRouter = require('./routes/event.js');
 const personRouter = require('./routes/person.js');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use('/assets', express.static(path.resolve(__dirname, '../client/assets')));
-
-
-// if (process.env.NODE_ENV === 'production') {
+// serve static files //
 app.use('/build', express.static(path.resolve(__dirname, '../build')));
+
+
 
 app.use('/add', personRouter);
 
@@ -22,16 +21,15 @@ app.use('/populate', eventRouter);
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../index.html'));
 })
-// }
 
 
-
-
+// Catch All Error Handler //
 app.use((req, res, next) => {
   console.log('Catch-all!!!!!')
-  res.status(404).send('Catch-all error, bb!!');
+  res.status(404).send('Not found!');
 })
 
+// Global Middleware Error Handler //
 app.use((err, req, res, next) => {
   const defaultErr = {
     log: 'Express error handler caught unknown middleware error',
@@ -43,5 +41,5 @@ app.use((err, req, res, next) => {
   return res.status(errorObj.status).json(errorObj.message);
 })
 
-
+// connects //
 app.listen(3000);
