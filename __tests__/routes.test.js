dea1f7badf05f2b64145b2b12da5c37f9c61f610
Matt Meigs <mattmeigs@gmail.com>
2021-02-02 17:42:11 -0500

    finished eventController test suite

--- __tests__/routes.test.js
+++ __tests__/routes.test.js
@@ -36,19 +36,12 @@ describe('Error handling', () => {
 });
 
 describe('Event Routes', () => {
-  it('get request for all events', async (done) => {
-    const res = await request(app)
-      .get('/populate')
-    expect(res.status).toBe(200);
-    expect(Array.isArray(res.body)).toBe(true);
-    done();
-  }, 10000);
 
   it('post to /populate', async (done) => {
     const res = await request(app)
       .post('/populate')
       .send({
-        events: [{time_start: '13:00:00', time_end: '15:00:00', location: 'Rehearsal Room', info: 'Bring score', name: ['Jessica Vosk']}],
+        events: [{time_start: '13:00:00', time_end: '15:00:00', location: 'Rehearsal RoomZZZ', info: 'Bring score', name: ['Jessica Vosk']}],
         day: 3,
         week: 3,
       })
@@ -56,10 +49,24 @@ describe('Event Routes', () => {
     done();
   });
 
-  // it('delete to /populate', async (done) => {
-  //   const res = await request(app)
-  //     .delete('/populate')
-  //   expect(res.status).toBe(200);
-  //   done();
-  // });
+  it('get request for all events', async (done) => {
+    const res = await request(app)
+      .get('/populate')
+    expect(res.status).toBe(200);
+    expect(Array.isArray(res.body)).toBe(true);
+    allEvents = res.body;
+    done();
+  }, 8000);
+
+  it('delete to /populate', async (done) => {
+    const res1 = await request(app)
+      .get('/populate')
+    const newEvent = res1.body.filter(event => event.location === 'Rehearsal RoomZZZ');
+    const { event_id } = newEvent[0];
+    const res2 = await request(app)
+      .delete('/populate')
+      .send({ id: event_id })
+    expect(res2.status).toBe(200);
+    done();
+  });
 });
