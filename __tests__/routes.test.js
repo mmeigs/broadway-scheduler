ef50783801149f263323870c2f95fa7ecc1977e8
Matt Meigs <mattmeigs@gmail.com>
2021-02-02 17:43:24 -0500

    cleaned up original test

--- __tests__/routes.test.js
+++ __tests__/routes.test.js
@@ -2,12 +2,6 @@ const request = require('supertest');
 const app = require('../server/app.js');
 
 
-describe('Sample Test', () => {
-  it('Assert True', () => {
-    expect(1 === 1).toBe(true);
-  });
-});
-
 describe('Static Files', () => {
   it('index.html served', async (done) => {
     const res = await request(app)
